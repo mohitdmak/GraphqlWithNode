2ee276ff7dcedea9fd8465cade28e42138cae423
mohitdmak <mdmohitrc@gmail.com>
2021-04-24 19:33:44 +0530

    testing with dummy data

--- app.js
+++ app.js
@@ -10,15 +10,84 @@ const { graphqlHTTP } = require('express-graphql')
 //var root
 
 // GraphQL schema
+// GraphQL schema
 var schema = graphql.buildSchema(`
     type Query {
-        message: String,
-        errmessage: String
+        course(id: Int!): Course
+        courses(topic: String): [Course]
+    },
+    type Mutation {
+        update(id: Int!, topic: String!): Course
+    }
+    type Course {
+        id: Int
+        title: String
+        author: String
+        description: String
+        topic: String
+        url: String
     }
 `);
+var coursesData = [
+    {
+        id: 1,
+        title: 'The Complete Node.js Developer Course',
+        author: 'Andrew Mead, Rob Percival',
+        description: 'Learn Node.js by building real-world applications with Node, Express, MongoDB, Mocha, and more!',
+        topic: 'Node.js',
+        url: 'https://codingthesmartway.com/courses/nodejs/'
+    },
+    {
+        id: 2,
+        title: 'Node.js, Express & MongoDB Dev to Deployment',
+        author: 'Brad Traversy',
+        description: 'Learn by example building & deploying real-world Node.js applications from absolute scratch',
+        topic: 'Node.js',
+        url: 'https://codingthesmartway.com/courses/nodejs-express-mongodb/'
+    },
+    {
+        id: 3,
+        title: 'JavaScript: Understanding The Weird Parts',
+        author: 'Anthony Alicea',
+        description: 'An advanced JavaScript course for everyone! Scope, closures, prototypes, this, build your own framework, and more.',
+        topic: 'JavaScript',
+        url: 'https://codingthesmartway.com/courses/understand-javascript/'
+    }
+]
+var getCourse = function(args) { 
+    var id = args.id;
+    return coursesData.filter(course => {
+        return course.id == id;
+    })[0];
+}
+var getCourses = function(args) {
+    if (args.topic) {
+        var topic = args.topic;
+        return coursesData.filter(course => course.topic === topic);
+    } else {
+        return coursesData;
+    }
+}
+
+var updateCourseTopic = function({id, topic}) {
+    coursesData.map(course => {
+        if (course.id === id) {
+            course.topic = topic;
+            return course;
+        }
+    });
+    return coursesData.filter(course => course.id === id) [0];
+}
+
+var root = {
+    course: getCourse,
+    courses: getCourses,
+    update: updateCourseTopic,
+};
 
 app.use('/graphpaths', graphqlHTTP({
     schema: schema,
+    rootValue: root,
     graphiql: true
 }));
 
